923b2bd8cf600134f1e3b38e111319cfc2690d57
Zyun Lam <zyunlam@gmail.com>
2016-12-28 16:20:17 -0600

    Update SystemLog.java
    Added a couple of other things, like starting a new log, and deleting the log file after it is too full

--- src/Zyun/Lam/Game/MonsterQuest/SystemLog.java
+++ src/Zyun/Lam/Game/MonsterQuest/SystemLog.java
@@ -1,6 +1,7 @@
 package Zyun.Lam.Game.MonsterQuest;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
@@ -22,8 +23,6 @@ public class SystemLog {
     static FileWriter logFileOutput = null;
     static BufferedWriter logFileWriter = null;
 
-    
-    
     public static void log (String text) {
         //Create Time Stamp
         Date timeStamp = new Date();
@@ -46,4 +45,51 @@ public class SystemLog {
             }
         }
     }
+    
+        public static void startNewLog () {
+        //Create Time Stamp
+        Date timeStamp = new Date();
+        String content = (" --- NEW LOG STARTED [" + timeStamp + "] ---");
+        try {
+            logFileOutput = new FileWriter("system.log", true);
+            logFileWriter = new BufferedWriter(logFileOutput);
+            logFileWriter.newLine();
+            logFileWriter.write(content);
+            logFileWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (logFileWriter != null)
+                    logFileWriter.close();
+                if (logFileOutput != null)
+                    logFileOutput.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+        
+        static void clearFile () {
+            File logFile = new File ("system.log");
+            //If log file does not exist, who cares, as we will make it later
+            if (logFile.exists()) {
+                if (logFile.length() > 102400) { //Size of 100 kb, will delete the file if so
+                    if (!logFile.delete()) {
+                        //Well, something is wrong, as it should be able to delete. Will there have a exception?
+                        //Anyway, report it
+                        System.err.println("Unable to delete log file");
+                    } else {
+                        try {
+                            logFile.createNewFile();
+                            log("Cleared file of things.");
+                            System.out.println("Zyun.Lam.Game.MonsterQuest.SystemLog.clearFile() : Cleared file of things");
+                        } catch (IOException ioe) {
+                            System.err.println("Unable to open file: " + ioe.getMessage());
+                            //Can't write to log files.
+                        }
+                    }
+                }
+            }
+        }
 }
