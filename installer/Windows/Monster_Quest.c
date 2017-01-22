94383e358f6f945dbe9dffe4635cf2713d68ec38
Zyun Lam <zyunlam@gmail.com>
2017-01-22 17:48:53 -0600

    Update Monster_Quest.c
    Added the basic framework

--- installer/Windows/Monster_Quest.c
+++ installer/Windows/Monster_Quest.c
@@ -2,5 +2,13 @@
 #include <stdlib.h>
 
 int main (void) {
+  FILE *fileResource;
+  if ((fileResource = fopen ("r", "resourcelist.txt")) != NULL) {
+    //read the whole file
+  }
+  else {
+    fprintf (stderr, "Unable to open the resource file");
+    perror ("You goofed!");
+  }
   return 0;
 }
