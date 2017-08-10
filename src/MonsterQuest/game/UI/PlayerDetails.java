f44de744f239de6afa378cad5ce39a631fac5beb
Zyun LAm <zyunlam@gmail.com>
2017-08-10 21:36:55 +0000

    Work on the website, and work on the player user interface.

--- src/MonsterQuest/game/UI/PlayerDetails.java
+++ src/MonsterQuest/game/UI/PlayerDetails.java
@@ -3,12 +3,19 @@ package MonsterQuest.game.UI;
 import MonsterQuest.game.painter.Paintable;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.Color;
 /**
- * 
+ * This is the implementation for the player details.
  * @author Zyun
  */
 public class PlayerDetails implements Paintable{
+    // All the rectangles for the wiki
+    Rectangle2D.Float userAvatar = new Rectangle2D.Float(50, 50, 100, 100);
     @Override
     public void paint (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.RED);
+        g2d.fill(userAvatar);
     }
 }
